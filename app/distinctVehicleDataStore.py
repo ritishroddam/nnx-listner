ab1650a3232643ec1120e98ceabc0310ec8a4da2
ritishroddam <ritishrajeshroddam@gmail.com>
2025-06-03 16:05:09 +0530

    Refactor distinctVehicleDataStore.py to remove WebSocket connection logic and clean_imei function; update sorting to use date_time

--- app/distinctVehicleDataStore.py
+++ app/distinctVehicleDataStore.py
@@ -11,33 +11,18 @@ import os
 
 sio = socketio.Client(ssl_verify=False)  # Disable verification for self-signed certs
 
-server_url = "https://cordonnx.com" 
-cert_path = os.path.join(os.path.dirname(__file__), "cert", "fullchain.pem")
-
-ssl_context = ssl.create_default_context(cafile=cert_path)
-
-try:
-    sio.connect(server_url, transports=['websocket'])
-    print("Connected to WebSocket server successfully!")
-except Exception as e:
-    print(f"Failed to connect to WebSocket server: {e}")
-
 mongo_client = MongoClient("mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin")
 db = mongo_client["nnx"]
 atlanta_collection = db['atlanta']
 distinct_atlanta_collection = db['distinctAtlanta']
 vehicle_inventory_collection = db['vehicle_inventory']
 
-def clean_imei(imei):
-    # Extract the last 15 characters of the IMEI
-    return imei[-15:]
-
 def update_distinct_atlanta():
     try:
         print("Successfully running distinct Vehicle")
         all_documents = list(atlanta_collection.aggregate([
                 {"$match": {"gps": "A", "status": "01"}},
-                {"$sort": {"timestamp": -1}},
+                {"$sort": {"date_time": -1}},
                 {
                     "$group": {
                         "_id": "$imei",
@@ -49,23 +34,13 @@ def update_distinct_atlanta():
         
         print(f"Fetched {len(all_documents)} documents from the atlanta collection")
 
-        # Fetch existing data from distinctAtlanta collection
-        existing_documents = {
-            doc['imei']: doc for doc in distinct_atlanta_collection.find()
-        }
-
-        # Group documents by IMEI and find the most recent document for each IMEI
-        distinct_documents = {}
         for doc in all_documents:
-            imei = clean_imei(doc['imei'])
             doc.pop('_id', None)
-            distinct_documents[imei] = {**doc, 'imei': imei}
 
         distinct_atlanta_collection.delete_many({})
 
-        # Insert the distinct documents into the distinctAtlanta collection
-        for doc in distinct_documents.values():
-            distinct_atlanta_collection.insert_one(doc)
+        if all_documents:
+            distinct_atlanta_collection.insert_many(all_documents)
 
         print('Distinct documents updated successfully')
 
