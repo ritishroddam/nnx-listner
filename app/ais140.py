8c59ede0b718cfb7edb13433e4683b7d9b166623
Ritish <ritishrajeshroddam@gmail.com>
2025-08-07 15:00:32 +0530

    Enhance debugging output in ais140.py by adding timestamps to log messages for better traceability.

--- app/ais140.py
+++ app/ais140.py
@@ -1,4 +1,5 @@
 import socket
+from datetime import datetime
 
 def start_server():
     host = '0.0.0.0'  # Listen on all available interfaces
@@ -7,24 +8,29 @@ def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((host, port))
         server_socket.listen(5)
-        print(f"Listening for connections on port {port}...")
+        print(f"[Debug {datetime.now()}]  Listening for connections on port {port}...")
 
         while True:
             client_socket, client_address = server_socket.accept()
-            print(f"Connection established with {client_address}")
+            print(f"[Debug {datetime.now()}] Connection established with {client_address}")
+
+            command = 'GPRSSTATUS'
+            
+            client_socket.sendall(command.encode('utf-8'))
+            
             with client_socket:
                 while True:
                     try:
                         data = client_socket.recv(1024)
                         if not data:
-                            print(f"Client {client_address} disconnected gracefully.")
+                            print(f"[Debug {datetime.now()}] Client {client_address} disconnected gracefully.")
                             break
-                        print(f"Received AIS 140 data: {data}")
+                        print(f"[Debug {datetime.now()}] Received AIS 140 data: {data}")
                     except ConnectionResetError:
-                        print(f"Client {client_address} disconnected unexpectedly.")
+                        print(f"[Debug {datetime.now()}] Client {client_address} disconnected unexpectedly.")
                         break
                     except Exception as e:
-                        print(f"Socket error with {client_address}: {e}")
+                        print(f"[Debug {datetime.now()}] Socket error with {client_address}: {e}")
                         break
 
 if __name__ == "__main__":
