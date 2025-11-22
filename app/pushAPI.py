17a48fb9eeb471a05667f5acc47251aced33c347
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2025-11-22 12:11:25 +0530

    Fix: Remove unnecessary space in moveInSyncSubscribed assignment for consistency

--- app/pushAPI.py
+++ app/pushAPI.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 mongo_client = MongoClient("mongodb+srv://doadmin:4T81NSqj572g3o9f@db-mongodb-blr1-27716-c2bd0cae.mongo.ondigitalocean.com/admin?tls=true&authSource=admin", tz_aware=True)
 db = mongo_client["nnx"]
 
-moveInSyncSubscribed = db ['moveInSyncSubscriptions']
+moveInSyncSubscribed = db['moveInSyncSubscriptions']
 
 getMoveInSyncAlerts = {
     'speed': 'OVERSPEEDING',
