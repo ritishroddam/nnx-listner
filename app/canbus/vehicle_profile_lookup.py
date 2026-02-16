12b95d65df561f7ade4f27d3b162f2a12811a96a
Ritish R Roddam <ritishrajeshroddam@gmail.com>
2026-02-16 15:38:08 +0530

    dsfad

--- app/canbus/vehicle_profile_lookup.py
+++ app/canbus/vehicle_profile_lookup.py
@@ -5,7 +5,7 @@ db = client.tracking
 vehicle_collection = db['vehicle_inventory']
 
 async def get_vehicle_profile(imei: str) -> str:
-    vehicle = await db.vehicles.find_one({"imei": imei})
+    vehicle = await vehicle_collection.find_one({"imei": imei})
     if vehicle and vehicle.get("vehicle_profile"):
         return vehicle["vehicle_profile"]
     return "generic_unknown"
